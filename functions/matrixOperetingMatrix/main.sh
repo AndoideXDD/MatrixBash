fc0e27194b088268c7021925095125f0ca2448d4
AndoideXDD <andreupugibert7@gmail.com>
2023-12-17 23:35:41 +0100

    The columns are rows and rows are columns

--- functions/matrixOperetingMatrix/main.sh
+++ functions/matrixOperetingMatrix/main.sh
@@ -2,9 +2,10 @@
 separador=$1
 file1=$2
 file2=$3
+result=""
 columns=$(tail -1 $file1 | awk  -F "$separador" '{print NF}')
 for ((colum=1;colum<=$columns; colum++ )) 
 do
     awk  -F "$separador" 'FNR==NR {a[FNR]=$'$colum'; next} {a[FNR]+=$'$colum'} 
-    END {for (i=1; i<=FNR; i++) {print a[i]}}' $file1 $file2
+    END {column=a[1] ; for (i=2; i<='$columns'; i++) {column=column","a[i]} print column}' $file1 $file2
 done
